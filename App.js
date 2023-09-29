869ed72ae117b5b8876e716d15f895c68ba50922
Unai <unai.heredero@ikasle.aeg.eus>
2023-09-29 12:00:20 +0200

    añadida tab bar con color cambiado

---
+++ App.js
@@ -0,0 +1,85 @@
+import React, {Component} from "react";
+import {StyleSheet, Text, View, Button} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Chats from './app/components/Chat';
+import States from './app/components/State';
+import Calls from './app/components/Call';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
+
+export default class App extends Component{
+ 
+  render() {
+    return(
+      <View style={styles.mainContainer}>
+        <View style={styles.headerContainer}>
+          <View style={styles.leftHeaderContainer}>
+            <Text style={styles.logo}>WhatsApp</Text>
+          </View>
+          <View style={styles.rightHeaderContainer}>
+            <Icon name="search" color="#fff" size={23} style={styles.icon}/>
+            <Icon name="more-vert" color="#fff" size={23} style={styles.icon}/>
+          </View>
+        </View>
+        <View style={styles.contentContainer}>
+       <NavigationContainer>
+            <Tab.Navigator
+              tabBarOptions={{
+                activeTintColor: 'black', // Change the color of the active tab text
+                inactiveTintColor: 'gray', // Change the color of the inactive tab text
+                indicatorStyle: {
+                  backgroundColor: '#075e54', // Cambia el color de la línea indicadora
+                },
+                style: {
+                  backgroundColor: '#075e54', // Change the background color of the tab bar
+                }
+              }}
+            >
+              <Tab.Screen name="Chats" component={Chats} />
+              <Tab.Screen name="States" component={States} />
+              <Tab.Screen name="Calls" component={Calls} />
+            </Tab.Navigator>
+          </NavigationContainer>
+
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+mainContainer: {
+  flex: 1,
+  backgroundColor: '#F5FCFF'
+},
+headerContainer:{
+  flex: 1,
+  flexDirection:"row",
+  justifyContent: "space-between",
+  backgroundColor: "#075e54",
+  alignItems: "center"
+},
+contentContainer: {
+  flex: 8,
+},
+rightHeaderContainer: {
+  flexDirection: "row",
+  alignItems: "flex-end"
+},
+leftHeaderContainer: {
+  flexDirection: "row",
+  alignItems: "flex-start"
+},
+logo: {
+  color: "#ffffff",
+  fontSize: 18,
+  marginLeft: 10,
+  fontWeight: "bold"
+},
+icon: {
+  padding: 5
+}
+});
